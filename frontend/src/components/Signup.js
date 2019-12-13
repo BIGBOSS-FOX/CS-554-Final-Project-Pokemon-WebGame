f1f6b9a872f8f6f1d101538c549dee124fd5808b
unknown <chendymaodai@gmail.com>
2019-12-13 16:16:18 -0500

    duplicate username blocked in signup page

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -3,12 +3,23 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signUp } from '../store/actions/authAction'
 import { Redirect } from 'react-router-dom'
+import { fetchAllUsers } from '../store/actions/fetchAction'
 
 class Signup extends Component {
     state = {
         username: '',
         email: '',
-        password: ''
+        password: '',
+        usernameExist: false
+    }
+
+    componentDidMount() {
+        console.log('Signup.js componentDidMount() props before fetchAllUsers()')
+        console.log(this.props)
+        this.props.fetchAllUsers();
+        // this.setState({
+        //     email: this.props.auth_firebase.email
+        // })
     }
 
     handleChange = (e) => {
@@ -20,11 +31,43 @@ class Signup extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         // console.log(this.state);
-        this.props.signUp(this.state)
+        for (let i = 0; i < this.props.fetch_mongodb.userList.length; i++) {
+            console.log("Signup handleSubmit() this.props.fetch_mongodb.userList[i].userName")
+            console.log(this.props.fetch_mongodb.userList[i].userName)
+            if (this.state.username.toLowerCase() === this.props.fetch_mongodb.userList[i].userName) {
+                // setTimeout(function(){
+                //     this.setState({
+                //         usernameExist: true
+                //     });
+                // },100)
+                this.setState({
+                    usernameExist: true
+                }, () => {
+                    console.log("Signup handleSubmit() this.state before breaking for loop ")
+                    console.log(this.state)
+                    if (!this.state.usernameExist) {
+                        console.log("Signup handleSubmit() fire signUp()")
+                        this.props.signUp(this.state)
+                    }         
+                });
+
+                break;
+            }
+        }
+
+        console.log("Signup handleSubmit() this.state after for loop ")
+        console.log(this.state)
+        // if (!this.state.usernameExist) {
+        //     console.log("Signup handleSubmit() fire signUp()")
+        //     this.props.signUp(this.state)
+        // }         
+            
     }
+
     render() {
-        const { auth_mongodb, auth_firebase } = this.props;
-        // console.log(this.props)
+        const { auth_mongodb, auth_firebase, fetch_mongodb } = this.props;
+        // console.log("Signup.js render() first user's userName")
+        // console.log(fetch_mongodb.userList[0])
         if (!auth_firebase.isLoaded) return <p>authenticating...</p>
         if (auth_firebase.uid) return <Redirect to= {'/user/' + auth_firebase.uid} />
         return (
@@ -44,9 +87,10 @@ class Signup extends Component {
                         <input type="password" id='password' onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-1">Signup</button>
+                        <button type="submit" className="btn pink lighten-1 z-depth-1">Signup</button>
                         <div className="red-text center">
                             { auth_mongodb ? <p>{auth_mongodb.authError}</p> : null}
+                            { this.state.usernameExist ? <p>Username already exists</p> : null}
                         </div>
                     </div>
                 </form>
@@ -67,13 +111,15 @@ const mapStateToProps = (state) => {
     // console.log(state);
     return {
         auth_firebase: state.firebase.auth,
-        auth_mongodb: state.auth
+        auth_mongodb: state.auth,
+        fetch_mongodb: state.fetch
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        signUp: (newUser) => dispatch(signUp(newUser))
+        signUp: (newUser) => dispatch(signUp(newUser)),
+        fetchAllUsers: () => dispatch(fetchAllUsers())
     }
 }
 
